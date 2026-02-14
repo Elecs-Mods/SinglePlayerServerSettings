7944ce33d0befd56e37f1aa686ffce95204730b5
Elec332 <6115944+Elec332@users.noreply.github.com>
2026-02-14 02:55:13 +0100

    Added support for all 1.21.x versions

--- src/main/java/nl/elec332/minecraft/singleplayerserversettings/mixin/ShareToLanScreenMixin.java
+++ src/main/java/nl/elec332/minecraft/singleplayerserversettings/mixin/ShareToLanScreenMixin.java
@@ -3,6 +3,7 @@ package nl.elec332.minecraft.singleplayerserversettings.mixin;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.CycleButton;
 import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.client.gui.components.Tooltip;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.ShareToLanScreen;
 import net.minecraft.client.server.IntegratedServer;
@@ -27,11 +28,11 @@ public abstract class ShareToLanScreenMixin extends Screen {
     }
 
     @Unique
-    private boolean onlineMode;
+    private CycleButton<Boolean> offlineMode;
     @Unique
-    private boolean pvpAllowed;
+    private CycleButton<Boolean> pvpAllowed;
     @Unique
-    private String motd;
+    private EditBox motd;
 
     @Unique
     private static final Component OFFLINE_MODE = Component.literal("Offline Mode");
@@ -53,27 +54,36 @@ public abstract class ShareToLanScreenMixin extends Screen {
     private void initHook(CallbackInfo ci) {
         IntegratedServer integratedServer = Objects.requireNonNull(Objects.requireNonNull(this.minecraft).getSingleplayerServer());
         this.setPort(25565);
-        this.motd = integratedServer.getMotd();
-        this.onlineMode = integratedServer.usesAuthentication();
-        this.pvpAllowed = integratedServer.isPvpAllowed();
+        this.pvpAllowed = CycleButton.onOffBuilder(true).create(this.width / 2 - 155, 130, 150, 20, PVP_ALLOWED);
+        try {
+            this.pvpAllowed.setValue(integratedServer.isPvpAllowed());
+        } catch (NoSuchMethodError e) {
+            this.pvpAllowed.active = false;
+            this.pvpAllowed.setTooltip(Tooltip.create(Component.literal("Setting pvp is not supported in this Minecraft version.")));
+        }
+        this.offlineMode = CycleButton.onOffBuilder(!integratedServer.usesAuthentication()).create(this.width / 2 + 5, 130, 150, 20, OFFLINE_MODE);
+
         this.getBox().setX(this.width / 2 + 5);
         this.getBox().setY(170);
-        this.addRenderableWidget(CycleButton.onOffBuilder(!this.onlineMode).create(this.width / 2 + 5, 130, 150, 20, OFFLINE_MODE, (cycleButton, b) -> this.onlineMode = !b));
-        this.addRenderableWidget(CycleButton.onOffBuilder(this.pvpAllowed).create(this.width / 2 - 155, 130, 150, 20, PVP_ALLOWED, (cycleButton, b) -> this.pvpAllowed = b));
-        EditBox motd = new EditBox(this.font, this.width / 2 - 155, 170, 150, 20, MOTD);
-        motd.setValue(this.motd);
-        motd.setResponder(s -> this.motd = s);
-        this.addRenderableWidget(motd);
+        this.addRenderableWidget(this.offlineMode);
+        this.addRenderableWidget(this.pvpAllowed);
+        this.motd = new EditBox(this.font, this.width / 2 - 155, 170, 150, 20, MOTD);
+        this.motd.setValue(integratedServer.getMotd());
+        this.addRenderableWidget(this.motd);
     }
 
     @Inject(method = {"lambda$init$2", "method_19851", "m_279789_"}, at = @At(value = "TAIL"), remap = false)
     private void onServerStart(CallbackInfo ci) {
         IntegratedServer integratedServer = Objects.requireNonNull(Objects.requireNonNull(this.minecraft).getSingleplayerServer());
-        integratedServer.setUsesAuthentication(onlineMode);
-        integratedServer.setPvpAllowed(pvpAllowed);
-        integratedServer.setMotd(this.motd);
-        this.minecraft.gui.getChat().addMessage(Component.empty().append(OFFLINE_MODE).append(": " + !onlineMode + "    ").append(PVP_ALLOWED).append(": " + pvpAllowed));
-        this.minecraft.gui.getChat().addMessage(Component.empty().append(MOTD).append(": " + this.motd));
+        integratedServer.setUsesAuthentication(!this.offlineMode.getValue());
+        try {
+            integratedServer.setPvpAllowed(this.pvpAllowed.getValue());
+        } catch (NoSuchMethodError e) {
+            // Later 1.21.x versions do this using per-world GameRules
+        }
+        integratedServer.setMotd(this.motd.getValue());
+        this.minecraft.gui.getChat().addMessage(Component.empty().append(OFFLINE_MODE).append(": " + this.offlineMode.getValue() + "    ").append(PVP_ALLOWED).append(": " + (this.pvpAllowed.active ? this.pvpAllowed.getValue() : "default")));
+        this.minecraft.gui.getChat().addMessage(Component.empty().append(MOTD).append(": " + this.motd.getValue()));
     }
 
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;drawCenteredString(Lnet/minecraft/client/gui/Font;Lnet/minecraft/network/chat/Component;III)V", ordinal = 1, shift = At.Shift.AFTER), cancellable = true)
